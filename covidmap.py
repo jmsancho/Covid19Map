73458762aa0186b063cb96fc8ce00000fe6fa6fe
jmsancho <50411042+jmsancho@users.noreply.github.com>
2020-03-29 11:56:23 -0300

    update series URL
    -URL of time series changed to newest version.
    -Export all images to a subfolder to avoid clutter.
    
    -Newer version of GIF map up to 2020-03-28

--- covidmap.py
+++ covidmap.py
@@ -8,22 +8,22 @@ import os
 from matplotlib.lines import Line2D
 
 
-covid = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-covid_d = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+covid = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+covid_d = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
-# Check output dir, create if it doesn't exist
-if not os.path.exists('out/'):
-    os.makedirs('out/') 
+#Check if img folder exists; if not, create it
+if not os.path.exists('img'):
+    os.makedirs('img') 
 
 ### CREATE ALL IMAGES
 for d in range(4,covid.iloc[1,:].size):
     print('Plotting Fig '+str(d))
     
-    # Figure dimensions
+    # Set the dimension of the figure
     my_dpi=96
     plt.figure(figsize=(2600/my_dpi, 1800/my_dpi), dpi=my_dpi)
     
-    # Make world map
+    # Make the background map
     m=Basemap(llcrnrlon=-180, llcrnrlat=-65,urcrnrlon=180,urcrnrlat=80)
     m.drawmapboundary(fill_color='#A6CAE0', linewidth=0)
     m.fillcontinents(color='grey', alpha=0.3)
@@ -31,7 +31,7 @@ for d in range(4,covid.iloc[1,:].size):
      
     
      
-    # Add points
+    # Add a point per position
     m.scatter(x=covid["Long"], y=covid["Lat"], s=covid[covid.iloc[:,d].name], alpha=0.4, color='yellow')
     m.scatter(x=covid_d["Long"], y=covid_d["Lat"], s=covid_d[covid_d.iloc[:,d].name], alpha=0.4, color='red')
     
@@ -44,29 +44,29 @@ for d in range(4,covid.iloc[1,:].size):
     plt.text( -177, -40,'COVID-19 Confirmed Cases/Deaths', ha='left', va='bottom', size=23, color='#555555' )
     plt.text( -177, -45,covid.iloc[:,d].name, ha='left', va='bottom', size=10, color='#555555' )
     
-    #Legend
     legend = [Line2D([0], [0], marker='o', color='w', label='Cases: '+str(totalc),
                           markerfacecolor='yellow', markersize=15),
               Line2D([0], [0], marker='o', color='w', label='Deaths: '+str(totald),
                           markerfacecolor='red', markersize=15)]
+    # plt.text( -170, -50,'Casos:'+str(totalc), ha='left', va='bottom', size=10, color='#555555' )
+    # plt.text( -170, -55,'Muertes:'+str(totald), ha='left', va='bottom', size=10, color='#555555' )
     
     plt.legend(handles=legend, loc=(0,0))
-    
     # Save as png
-    filename = 'out/' + str(d/10) + '.png'
-    plt.savefig(filename, bbox_inches='tight')
+    filename = str(d/10) + '.png'
+    plt.savefig('img/'+filename, bbox_inches='tight')
     plt.clf()
     
     
 ### CREATE GIF
 images = []
-for file_name in sorted(os.listdir('out/')):
+for file_name in sorted(os.listdir('img/')):
     if file_name.endswith('.png'):
-        images.append(io.imread('out/'+file_name))
+        images.append(io.imread('img/'+file_name))
         
-#ADD DUPLICATED END FRAMES
+#ADD END FRAMES
 for i in range(20):
     images.append(images[-1])
     
 #Save GIF
-io.mimsave('animated.gif', images,fps=7)
+io.mimsave('animated.gif', images,fps=5)
